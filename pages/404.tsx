64455354b80a29040b50b56a7c94d6158bb07455
“CharlesCahill123” <charles.cahill.intern@sofwerx.org>
2023-11-13 12:17:14 -0500

    404

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,9 +5,8 @@ import dynamic from "next/dynamic";
 
 export default function Notfound() {
     return (
-        <div>
-            <Title404/>
-            <Custom404/>
+        <div style={{height: 900}}>
+            <h1 className="mt-auto mx-auto mt-5" style={{textAlign: 'center', justifyContent: 'center', alignSelf: 'center', marginTop: 150}}>404 - Page Not Found</h1>
         </div>
     );
 };
