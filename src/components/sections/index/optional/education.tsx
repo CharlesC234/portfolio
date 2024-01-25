3919511348129c5ab055fe37e73d076be9d30248
“CharlesCahill123” <charles.cahill.intern@sofwerx.org>
2024-01-24 23:16:29 -0500

    Added anticipated graduation

--- src/components/sections/index/optional/education.tsx
+++ src/components/sections/index/optional/education.tsx
@@ -25,7 +25,7 @@ export default function Education() {
                         <div className={career.companyContent}>
         <span className={career.companyHeader}>
             <h3>Florida Polytechnic University</h3>
-            <h5>Lakeland, Florida</h5>
+            <h5>Lakeland, Florida · August 2021 - May 2025</h5>
         </span>
                             <p style={{fontSize: '15px'}}>During my time at Florida Polytechnic I have learned a tremendous amount. Initially I studied Electrical Engineering, but eventually switched my major, as I found that Computer Science aligned more with what I really wanted to do as well as my dreams of entrepreneurship. I would highly recommend the Computer Science program at Florida Polytechnic, I have had great hands on experience in the field, and amazing professors. Overall I feel it has adequately preppared me for the industry.</p>
 
